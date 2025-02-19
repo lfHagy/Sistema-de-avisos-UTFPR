359ef25dd6a7f9408a0f90663c35e514a61c155b
Luiz Felipe Hagy <144378168+lfHagy@users.noreply.github.com>
2025-02-19 11:29:00 -0300

    added token-based timeout, ensured active users list doesn't get any dupe users

--- src/server/routes/login.js
+++ src/server/routes/login.js
@@ -10,31 +10,41 @@ router.post("/", async (req, res) => {
   try {
     const { email, senha } = req.body;
 
-    if (typeof senha !== 'string') {
-      return res.status(400).json({ mensagem: "Senha precisa ser uma string" });
-    }
-
     const usuario = await Usuario.findOne({ email });
     if (!usuario || usuario.senha !== senha) {
-      return res.status(401).json({ mensagem: "Email e/ou senha invalidos" });
+      return res.status(401).json({ mensagem: "Email e/ou senha inválidos" });
     }
 
     const token = jwt.sign(
-      { email: usuario.email, admin: usuario.admin, exp: Math.floor(Date.now() / 1000) + 15 * 60 },
+      { email: usuario.email, admin: usuario.admin, exp: Math.floor(Date.now() / 1000) + 10 * 60 },
       secretKey,
-      { algorithm: 'HS256' }
+      { algorithm: "HS256" }
     );
+    // evitar que o usuário se duplique e guardar a nova token
+    const index = activeUsers.findIndex(user => user.email === email);
+    if (index !== -1) {
+      clearTimeout(activeUsers[index].timeout);
+      activeUsers.splice(index, 1);
+    }
 
-    activeUsers.push({ email: usuario.email, token });
+    const timeout = setTimeout(() => {
+      const removeIndex = activeUsers.findIndex(user => user.email === email);
+      if (removeIndex !== -1) {
+        activeUsers.splice(removeIndex, 1);
+        console.warn(`Token do usuário expirou: ${email}`);
+      }
+    }, 15 * 60 * 1000);
+
+    activeUsers.push({ email, token, timeout });
 
     res.status(200).json({ token });
   } catch (err) {
-    console.error("Error during login:", err);
+    console.error("Erro no login:", err);
     res.status(500).json({ mensagem: "Erro no servidor", error: err.message });
   }
 });
 
 module.exports = {
-  router, 
+  router,
   activeUsers
 };
